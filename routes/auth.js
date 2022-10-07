5f41b153ae2633a52062b630deca5b3bc9ebe677
abhishek <abhisheksonwane57@gmail.com>
2022-10-07 23:48:20 +0530

    encrypted password &cart UI fixes

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ const User = require('../models/User')
 const path = require('path')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt')
 
 
 const bodyParser = require('body-parser');
@@ -30,8 +32,6 @@ router.post('/register', async (req,res)=>{
     const existusername = await User.findOne({username:req.body.username})
     const existemail = await User.findOne({email:req.body.email})
     if(existusername){
-        // err_msg = "This user already exists.";
-        //     return res.render('../views/login/register', { err_msg: err_msg } );
         err_msg = 'this user already exist'
         return res.render('../views/login/register',{err_msg :err_msg}) 
     }
@@ -42,21 +42,24 @@ router.post('/register', async (req,res)=>{
         return res.render('../views/login/register',{err_msg :err_msg}) 
     }
     
-    const newUser =   new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
     try{
+        const hashedpassword = await bcrypt.hash(req.body.password,10)
+
+        const newUser =   new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedpassword
+        })
         const savedUser = await newUser.save();
         // res.status(201).json(savedUser);
         res.redirect('/auth/login')
+        // req.session.isAuth = true
+        // res.redirect('/catalogue')
     }
     catch(err){
         res.status(500).json(err);
     } 
 })
-
 
 
 
@@ -97,26 +100,14 @@ router.post('/login', async (req,res)=>{
     // !user && res.status(401).json("wrong credentials")
     // reqpassword !== req.body.password &&
     //     res.status(401).json("wrong credentials");
-    if(req.body.password == user.password){
+    // if(req.body.password ==user.password)
+    if(bcrypt.compare(req.body.password,user.password)){
                             const username = req.body.username;
                             const pass = req.body.password;
                             const uzer = {name:username,passw:pass}
-                            // const accesstoken = jwt.sign(uzer,process.env.ACCESS_TOKEN)
-                            // res.set({
-                            //     'content-type': 'application/json',
-                            //     'authorization':  `bearer ${accesstoken}`
-                            // })
-                            // return res.json({accesstoken:accesstoken})
-    
-            // const state = await User.updateOne({username:req.body.username},{"$set":{"isLoggedin":true}}).then(dbModel => res.json(dbModel))
-            // .catch(err => res.status(422).json(err));;
-            // console.log(state)
-            // const authToken = generateAuthToken();
-            // authTokens[authToken] = uzer;
-            // res.cookie('AuthToken', authToken);
+                           
 
-            // console.log(uzer)
-            if(username == 'admin'&&pass=='6193'){
+            if(username == process.env.ADMIN_AUTH_CREDENTIAL_USERNAME &&pass==process.env.ADMIN_AUTH_CREDENTIAL_PASSWORD){
                 req.session.isAdmin = true;
             }
             req.session.isAuth = true;
@@ -127,13 +118,15 @@ router.post('/login', async (req,res)=>{
     // const {password, ...others} = user._doc;
     // console.log(reqpassword)
     else {
-        
-    return res.status(401).sendFile(path.join(__dirname,'../L.jpg'))
+        err_msg = 'wrong username or password'
+        return  res.render("../views/login/login",{err_msg:err_msg})
     }
     // res.status(200).json(others)
    
     }catch(err){
         console.log(err)
+        err_msg = 'something went wrong'
+        res.render("../views/login/login",{err_msg:err_msg})
         // return res.redirect('/auth/login')
     }
 
